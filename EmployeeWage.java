adf1fa09b4adbe4b8b7668397f666b628d8b981a
touhidpasha <touhidpasha552@gmail.com>
2021-08-18 17:06:14 +0530

    employee present/absent checked

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,6 +1,13 @@
 public class EmployeeWage{
     public static void main(String[] args) {
         System.out.println("welcome to employee wage problem");
+
+        final int PRESENT = 1;
+        int var = (int) Math.floor(Math.random() * 10) % 2;
+        if (var == PRESENT)
+            System.out.println(" Employee is Present");
+        else
+            System.out.println(" Employee is Absent");
         
     }
     }
