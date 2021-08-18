09072ad78a0ad55a33942373dacce6e77f2dea59
touhidpasha <touhidpasha552@gmail.com>
2021-08-18 17:50:13 +0530

    daily wage calculated

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,12 +2,16 @@ public class EmployeeWage{
     public static void main(String[] args) {
         System.out.println("welcome to employee wage problem");
 
-        final int PRESENT = 1;
+        final int PRESENT = 1,wage_per_hour=20,full_day_duration=8;
         int var = (int) (Math.random() * 10) % 2;
-        if (var == PRESENT)
+        if (var == PRESENT){
             System.out.println(" Employee is Present");
+            int daily_wage=wage_per_hour*full_day_duration;
+            System.out.println(daily_wage);
+            }
         else
             System.out.println(" Employee is Absent");
+
         
     }
     }
