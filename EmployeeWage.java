4ba6aede930c1c5f1ec99b5c11c2c6c2dd8e2030
touhidpasha <touhidpasha552@gmail.com>
2021-08-18 17:58:26 +0530

    part time hour added

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,8 +2,9 @@ public class EmployeeWage{
     public static void main(String[] args) {
         System.out.println("welcome to employee wage problem");
 
-        final int PRESENT = 1,wage_per_hour=20,full_day_duration=8;
+        final int PRESENT = 1,wage_per_hour=20,full_day_duration=8,part_time_hout=4;//here i took part time hour as 4
         int var = (int) (Math.random() * 10) % 2;
+        
         if (var == PRESENT){
             System.out.println(" Employee is Present");
             int daily_wage=wage_per_hour*full_day_duration;
