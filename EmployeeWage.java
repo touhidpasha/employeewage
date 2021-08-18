eb87f550463641947119f7d9fee9ed09540962c3
touhidpasha <touhidpasha552@gmail.com>
2021-08-18 18:16:47 +0530

    total wage per month calculated

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -2,7 +2,7 @@ public class EmployeeWage{
     public static void main(String[] args) {
         System.out.println("welcome to employee wage problem");
 
-        final int PRESENT = 1,wage_per_hour=20,full_day_duration=8,part_time_hout=4;//here i took part time hour as 4
+        final int PRESENT = 1,wage_per_hour=20,full_day_duration=8,part_time_hout=4,total_working_days=20;//here i took part time hour as 4
         int var = (int) (Math.random() * 10) % 2,full_time=0;
         int daily_wage;
         if (var == PRESENT){
@@ -11,12 +11,12 @@ public class EmployeeWage{
             switch(full_time)
             {
                 case(1):
-                    daily_wage=wage_per_hour*full_day_duration;
+                    daily_wage=wage_per_hour*full_day_duration*total_working_days;
                     System.out.println(daily_wage);
                     
                     break;
                 case 0:
-                    daily_wage=part_time_hout*wage_per_hour;
+                    daily_wage=part_time_hout*wage_per_hour*total_working_days;
                     System.out.println(daily_wage);
                     break;
 
